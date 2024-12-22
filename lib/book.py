6338ae2816d864b5ea15340a25aaeb760392c9c6
willisntannpc <wn.mugoto@gmail.com>
2024-12-22 05:47:07 +0300

    tests passed

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,23 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
+    def __init__(self, title, author, genre):
+        self.title = title
+        self.author = author
+        self.genre = genre
+
+    def __repr__(self):
+        return f"Book(title={self.title}, author={self.author}, genre={self.genre})"
+
+    @property
+    def title(self):
+        return self._title
+
+    @title.setter
+    def title(self, value):
+        if not value:
+            raise ValueError("Title cannot be empty")
+        self._title = value
+
     
         
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,20 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self, brand, size, color):
+        self.brand = brand
+        self.size = size
+        self.color = color
+
+    def __repr__(self):
+        return f"Shoe(brand={self.brand}, size={self.size}, color={self.color})"
+
+    @property
+    def size(self):
+        return self._size
+
+    @size.setter
+    def size(self, value):
+        if value <= 0:
+            raise ValueError("Size must be positive")
+        self._size = value
